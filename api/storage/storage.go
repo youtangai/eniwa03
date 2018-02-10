f3c36de9298a419aebcd7a1813daab66e72bdcb6
youtangai <youtanagai@gmail.com>
2018-02-10 21:53:11 +0900

    gormを除外

--- api/storage/storage.go
+++ api/storage/storage.go
@@ -1,18 +1,17 @@
 package storage
 
 import (
-	"errors"
 	"log"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql" // mysql driver
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver
 	"github.com/youtangai/eniwa03/api/config"
-	"github.com/youtangai/eniwa03/api/model"
 )
 
 var (
 	// DataBase is struct for db
-	DataBase *gorm.DB
+	DataBase *sql.DB
 )
 
 const (
@@ -22,34 +21,19 @@ const (
 
 func init() {
 	DataBase = connection()
-	//DataBase.SingularTable(true)
 }
 
-func connection() *gorm.DB {
+func connection() *sql.DB {
 	user := config.DBUser()
 	pass := config.DBPasswd()
 	host := config.DBHost()
 	port := config.DBPort()
 	dbName := config.DBName()
 	connectionString := user + ":" + pass + "@" + "tcp(" + host + ":" + port + ")/" + dbName + "?parseTime=true"
-	db, err := gorm.Open(DBMS, connectionString)
+	db, err := sql.Open(DBMS, connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return db
-}
-
-func createTable() error {
-	DataBase.AutoMigrate(&model.User{}, &model.Group{}, &model.UserGroup{})
-	if !DataBase.HasTable(&model.User{}) {
-		return errors.New("database:User table not created")
-	}
-	if !DataBase.HasTable(&model.Group{}) {
-		return errors.New("database:Group table not created")
-	}
-	if !DataBase.HasTable(&model.UserGroup{}) {
-		return errors.New("database:UserGroup table not created")
-	}
-	return nil
 }
