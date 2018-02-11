1149a6e676590d16f6f91d29e8cee9c4fc48f99c
youtangai <youtanagai@gmail.com>
2018-02-11 13:43:51 +0900

    コメント付与

--- api/model/response.go
+++ api/model/response.go
@@ -1,16 +1,19 @@
 package model
 
+//Login is
 type Login struct {
 	Status string `json:"status"`
 	UserID string `json:"user_id"`
 }
 
+//JoinedGroup is
 type JoinedGroup struct {
 	ID        int    `json:"id"`
 	GroupName string `json:"g_name"`
 	Status    string `json:"status"`
 }
 
+//Individual is
 type Individual struct {
 	UserID   int    `json:"user_id"`
 	UserName string `json:"user_name"`
@@ -18,6 +21,7 @@ type Individual struct {
 	Goal     int    `json:"goal"`
 }
 
+//GroupDetail is
 type GroupDetail struct {
 	State       int          `json:"state"`
 	Start       string       `json:"start"`
@@ -25,6 +29,7 @@ type GroupDetail struct {
 	Individuals []Individual `json:"individual"`
 }
 
+//GoalDetail is
 type GoalDetail struct {
 	Price int    `json:"price"`
 	Desc  string `json:"description"`
